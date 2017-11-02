9eff74b8c1e80432f8e3e0fc16a03a888c50c843
Yang <pccettfly@outlook.com>
2017-11-02 09:30:01 +0800

    update readme

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
-﻿**依赖环境：python3、google chrome**
-
-**1.安装依赖**
+﻿#依赖环境：#
+请安装python3、google chrome
+##**1.安装依赖**##
 
     pip install -r requirements.txt
 
-**2.编辑users.py，在ACCOUNT=和PASSWORD=的单引号中分别填入你的微博账号、密码**
-**3.安装mongodb和robomongo**
+##**2.编辑users.py**##
+在ACCOUNT=和PASSWORD=的单引号中分别填入你的微博账号、密码
+##**3.安装mongodb和robomongo**##
 可参考这篇文章：[Win10 MongoDB安装][1]
-安装成功并启动mongodb后，在浏览器中输入http://localhost:27017/,可观察到:
+**安装成功并启动mongodb后，在浏览器中输入http://localhost:27017/,可观察到:**
 > It looks like you are trying to access MongoDB over HTTP on the native driver port.
 
 **4.运行scrapy**
@@ -16,9 +17,23 @@
  
 
     `scrapy crawl weibotv`  ，程序将运行spiders目录下的wbtv.py
-    `scrapy crawl links`  ，程序将运行spiders目录下的links
+    `scrapy crawl links`  ，程序将运行spiders目录下的links.py
     `scrapy crawl relation`  ，程序将运行spiders目录下的relation.py
+#程序调试说明#
+以[JetBrains PyCharm Community Edition 2017.2.3][2]为例。
+**main.py文件中**，
+对应内容分别为
+ 1. `cmdline.execute("scrapy crawl weibotv".split())`此时为从wbtv.py开始调试
+ 2. `cmdline.execute("scrapy crawl links".split())`此时为从links.py开始调试
+ 3. `cmdline.execute("scrapy crawl relation".split())`此时为从links.py开始调试
+以调试wbtv.py为例。
+在wbtv中设置断点后，在pycharm中**debug执行main.py**,即可开始断点运行。断点运行后可开始单步调试。
+未获取cookies.json时，wbtv会调用web_driver.py文件，所以在web_driver.py中设置断点后，debug执行main.py同样可以断点调试或者单步调试web_driver.py文件。
+
+**同样的，按1、2、3分别修改main.py后，debug main.py时能分别对wbtv.py、links.py、relation.py及其调用的其他.py文件进行调试。**
+
+    
 
 
   [1]: http://www.jianshu.com/p/d6c7adfe45cf
-  
+  [2]: https://www.jetbrains.com/pycharm/download/#section=windows
--- web_driver.py
+++ web_driver.py
@@ -5,33 +5,33 @@ from users import ACCOUNT, PASSWORD
 
 class GetCookies:
     def __init__(self):
-        self.driver = webdriver.Chrome('C:/Users/akaza/Documents/URPPC/WeiboTV/weibo/chromedriver/chromedriver.exe')
+        self.driver = webdriver.Chrome('./weibo/chromedriver/chromedriver.exe') #相对路径调用weibo/chrome/下的chromedriver
         #self.driver.maximize_window()
-        self.driver.get('http://weibo.com/login.php')
-        time.sleep(10)
+        self.driver.get('http://weibo.com/login.php') #跳转微博首页
+        time.sleep(3)
         self.run()
 
     def run(self):
         try:
-            self.driver.find_element_by_xpath('//div[@class="info_list username"]/div/input').send_keys(ACCOUNT)
+            self.driver.find_element_by_xpath('//div[@class="info_list username"]/div/input').send_keys(ACCOUNT)    #找到用户名的输入框，自动输入用户名
             print('input username')
         except:
             print('username error!')
         time.sleep(3)
         try:
-            self.driver.find_element_by_xpath('//div[@class="info_list password"]/div/input').send_keys(PASSWORD)
+            self.driver.find_element_by_xpath('//div[@class="info_list password"]/div/input').send_keys(PASSWORD)   #自动输入密码
             print('input password')
         except:
             print('password error!')
-        time.sleep(3)
+        time.sleep(10)   #暂停十秒，如遇到验证码，请在十秒内输入
         try:
-            self.driver.find_element_by_xpath('//div[@class="info_list login_btn"]/a').click()
+            self.driver.find_element_by_xpath('//div[@class="info_list login_btn"]/a').click()  #自动点击登录按钮
             print('click to login')
         except:
             print('click error!')
         time.sleep(10)
         cookies={"cookies":[]}
-        for item in self.driver.get_cookies():
+        for item in self.driver.get_cookies():  #获取cookies，并写入本地，命名为cookies.json
             cookies["cookies"].append({"name":item["name"], "value":item["value"]})
         cookies["cookies"]=list(cookies["cookies"])
         file = open('cookies.json', 'w', encoding='utf-8')
