f2ff1ec9dc8f21e99cbce8c828fa2915d69df504
Yang <pccettfly@outlook.com>
2018-03-15 22:55:37 +0800

    update

--- extract_tags.py
+++ extract_tags.py
@@ -1,37 +1,20 @@
-import jieba.analyse
-import time
-from database_utils import ConnectDB
-from values import DATABASE_TV, COLLECTION_ITEM, DATE_FORMAT, REMOVE_CHAR
-
-
+import jieba.analyse,time
+from pymongo import MongoClient
+DB_Name = 'WeiboTV'
+Collection_Name = 'WeiboItem'
+DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+REMOVE_CHAR = {"[", "]", "'", "回复", "："}
 class ExtractTags:
-    client = None
-    database = None
-    collection = None
-
     def __init__(self):
-        self.client = ConnectDB(DATABASE_TV, COLLECTION_ITEM)
-        self.database, self.collection = self.client.get_handler()
-        jieba.enable_parallel(5)
+        self.client=MongoClient()
+        self.database=self.client[DB_Name]
+        self.collection=self.database[Collection_Name]
+        #jieba.enable_parallel(5)   #parallel模式，不支持windows
 
-    def remove_duplicate(self):
-        all, find, delete= 0, 0, 0
-        a = self.collection.distinct('url')
-        for i in range(self.collection.count(),0,-1):
-            all += 1
-            url = self.collection.find()[i-1]["url"]
-            if url in a:
-                a.remove(url)
-                find += 1
-            elif url not in a:
-                self.collection.remove(self.collection.find()[i-1])
-                delete += 1
-            print("Unique:%d\t\tDelete:%d\t\tProcess:%d\t\t" % (find, delete, all))
-
-    def remove_items(self):
+    def remove_items(self): #去除评论数小于50的视频，放入remove_items表
         items = self.collection.find({"$where": "this.comments.length  < 50" })
         for item in items:
-            self.database.get_collection('remove_items').insert({"url" : item["url"]})
+            self.database.get_collection('remove_items').insert_one(item)
             self.collection.remove({"url":item["url"]})
 
     def cut_comments(self):
@@ -46,8 +29,8 @@ class ExtractTags:
                     break
             for char in REMOVE_CHAR:
                 comments = comments.replace(char, "")
-            # cut_a = '/'.join(jieba.cut_for_search(comments)) # search engine mode
-            cut_a = '/'.join(jieba.cut(comments, cut_all=False)) # accurate mode
+            cut_a = '/'.join(jieba.cut_for_search(comments)) # search engine mode
+            # cut_a = '/'.join(jieba.cut(comments, cut_all=False)) # accurate mode
             # cut_a = '/'.join(jieba.cut(comments, cut_all=True)) # full mode
             self.collection.update({"url": item["url"]}, {"$unset": {"comments": ""}})
             self.collection.update({"url": item["url"]}, {"$set": {"comments": cut_a}})
@@ -64,7 +47,6 @@ class ExtractTags:
         self.client.close()
 
 pro = ExtractTags()
-pro.remove_duplicate()
-pro.remove_items()
+#pro.remove_items()
 pro.cut_comments()
 pro.close()
